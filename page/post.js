4ace64e7c1b0df5f0627fb046bd724b11ed08b31
Yebin Kim <dpqls161@gmail.com>
2026-04-03 03:16:57 +0900

    [0403] 에브리타임 JS 디벨롭

--- page/post.js
+++ page/post.js
@@ -1,7 +1,9 @@
-const textsubmit = document.querySelector('.textsubmit');
-if(textsubmit){
-    textsubmit.addEventListener("click", function(e){
-    window.location.href="post.html";    
+const writeIcon = document.querySelector('.textsubmit');
+const textBox1 = document.querySelector('.textBox1');
+if(writeIcon){
+    writeIcon.addEventListener("click", function(e){
+    textBox1.style.display="flex";
+    writeIcon.style.display="none";
 });
 }
 
@@ -16,17 +18,60 @@ if(article1){
 
 const hashTag = document.querySelector('.hashTag');
 const text_Content = document.querySelector('.text_Content');
-hashTag.addEventListener("click", function(e){
-    text_Content.value += "#"; 
-    text_Content.focus();
-});
+if (hashTag && text_Content) {
+    hashTag.addEventListener("click", function () {
+        text_Content.value += "#";
+        text_Content.focus();
+    });
+}
 
 const questionBox = document.querySelector('#questionBox');
 const questionContainer = document.querySelector('.questionContainer');
-questionBox.addEventListener("change",function(){
-    if (questionBox.checked){
-        questionContainer.style.display = "flex";
-    }else{
-                questionContainer.style.display = "none";
-    }
-});
+if (questionBox && questionContainer) {
+    questionBox.addEventListener("change", function () {
+        if (questionBox.checked) {
+            questionContainer.style.display = "flex";
+        } else {
+            questionContainer.style.display = "none";
+        }
+    });
+}
+
+let reaction = {
+    like : document.querySelector('.reaction_button1'),
+    likeNum : document.querySelector('.redTxt'),
+    scrap : document.querySelector('.reaction_button2'),
+    scrapNum : document.querySelector('.yellowTxt'),
+    scrapTxt : document.querySelector('.grayTxt'),
+    scrapImg: document.querySelector('.reaction_button2 img'),
+};
+
+let likeClick=false;
+let scrapNumClick = false;
+
+if (reaction.like && reaction.likeNum) {
+    reaction.like.addEventListener("click", function () {
+        if(likeClick) return;
+        reaction.likeNum.textContent = Number(reaction.likeNum.textContent) + 1;
+        likeClick=true;
+    });
+}
+if(reaction.scrap && reaction.scrapNum && reaction.scrapTxt){
+    reaction.scrap.addEventListener("click", function(){
+        if(!scrapNumClick){
+            reaction.scrapNum.textContent = Number(reaction.scrapNum.textContent)+1;
+            reaction.scrapTxt.textContent = "스크랩취소";
+            reaction.scrapImg.style.display = "none";
+            
+            scrapNumClick=true;
+        }
+        else{
+            reaction.scrapNum.textContent= Number(reaction.scrapNum.textContent)-1;
+            reaction.scrapTxt.textContent = "스크랩"; 
+            reaction.scrapImg.style.display = "inline";
+            
+            scrapNumClick=false;
+        }
+
+    });
+}
