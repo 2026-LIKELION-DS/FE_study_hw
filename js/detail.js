350c54257ad0713d73d32f935002d872e0c453a4
Yebin Kim <dpqls161@gmail.com>
2026-04-08 21:49:53 +0900

    [0408] 에브리타임 댓글 기능 구현

---
+++ js/detail.js
@@ -0,0 +1,68 @@
+const inputBox = document.querySelector(".comentbox");
+const submitBtn = document.querySelector(".writeButton");
+const addComent = document.querySelector(".sectionComment");
+const borderTag = document.querySelector(".sectionContent");
+const borderTag2 = document.querySelector(".comentContainer");
+const dbcoment = document.querySelector(".jscoment");
+
+let cnt = 2;
+submitBtn.addEventListener("click", function() {
+    let comentTxt=inputBox.value;
+    if (comentTxt.length>0){
+        createComent(comentTxt);
+        borderTag.style.borderBottom = "none";
+        borderTag2.style.borderTop="none";
+        
+        cnt++;
+        inputBox.value="";
+    }
+});
+
+function createComent(text){
+    addComent.innerHTML += `
+        <div class="sectionComment active">
+            <div class="coment-box">
+                <div class="writer">
+                    <div class="writerImg"><img src="../img/anonymous.profile.png" width="20" height="20">
+                    </div>
+                    <div class="writerAbout2">
+                    <p class="writername2">익명${cnt}</p>
+                    </div>
+                </div>
+                <ul class="comentWriter">
+                    <li class="sectionRight-menu jscoment">대댓글</li>
+                    <li class="sectionRight-menu">공감</li>
+                    <li class="sectionRight-menu">쪽지</li>
+                    <li class="sectionRight-menu">신고</li>
+                </ul>
+            </div>
+            <div class="comentValue">
+                ${text}
+                <p class="writeTime">04/05 21:30</p>
+            </div>
+        </div>
+    `;
+}
+function dbcomentBox(){
+    const div = document.createElement("div");
+    div.className="comentContainer";
+    div.innerHTML = `
+            <input type="text" class="comentbox" placeholder="대댓글을 입력하세요"></input>
+            <ul class="textfunction">
+                <li class="check-coment">
+                    <input type="checkbox" class="anonymousBox">익명
+                </li>
+                <li class="writeButton">
+                    <img src="../img/container.articles.write.submit.png"width="40px"height="40px" alt="글작성버튼">
+                </li>
+            </ul>
+    `;
+    return div;
+}
+document.addEventListener("click", function(e){
+    if(e.target.classList.contains("jscoment")){
+        const commentBox = e.target.closest(".sectionComment");
+        const replyBox = dbcomentBox();
+        commentBox.appendChild(replyBox); 
+    }
+});
