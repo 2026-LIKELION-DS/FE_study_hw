728c18b7b53db7350de2767586e42a04b471420e
stopwonee <stopw0n@naver.com>
2026-04-26 18:23:42 +0900

    [0426] 에브리타임 댓글 기능 구현

--- everytime/js/detail.js
+++ everytime/js/detail.js
@@ -9,10 +9,12 @@ const scrap_display_num = document.querySelector(".scrap_count");
 const rethatguel_btn = document.getElementById("reReply");
 const rethatguel_popup = document.querySelector(".boxForHide");
 
-const thatguel = document.querySelector(".thatguelChang");
 const rethatguel = document.querySelector(".reThatguelChang");
+const rethatguel_submit = document.querySelector(".submit_comment_rtg");
+const rethatguel_input = document.querySelector(".reThatguelChang")
+
+const thatguel = document.querySelector(".thatguelChang");
 const thatguel_submit = document.querySelector(".submit_comment_tg");
-const rethatguel_submit = document.querySelector(".submit_comment_rtg")
 
 let tta_count = 0;
 let scr_count = 0;
@@ -51,9 +53,25 @@ rethatguel_btn.addEventListener("click", function() {
     } else {
         rethatguel_popup.style.display = "flex";
     }
-})
+});
 
 rethatguel_submit.addEventListener("click", function() {
-    rethatguel.value = reThatguelContent;
-    alert(reThatguelContent);
-})
+    const reThatguelContent = rethatguel_input.value;
+
+    if (reThatguelContent){
+        alert(reThatguelContent);
+        rethatguel_input.value = "";
+    } else {
+        alert("대댓글 내용을 입력해주세요!")
+    }
+});
+
+thatguel_submit.addEventListener("click", function() {
+    const content = thatguel.value; 
+    if (content) {
+        alert(content);
+        thatguel_input.value = "";
+    } else {
+        alert("댓글 내용을 입력해주세요!");
+    }
+});
